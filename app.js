66316318e99cd32b751c92e0b474ea93b4adb25b
wbaig2 <w.baig321@gmail.com>
2022-08-02 10:50:30 +0100

    add ncnews-4

--- app.js
+++ app.js
@@ -1,14 +1,22 @@
 const express = require('express');
 const app = express();
-const { getTopics } = require('./controllers/app.controller')
-
-app.use(express.json());
+const { getTopics, getArticleById } = require('./controllers/app.controller')
 
 app.get('/api/topics', getTopics);
 
-app.all('/*', (req, res) => {
+app.get('/api/articles/:article_id', getArticleById);
+
+app.all("/*", (req, res) => {
   res.status(404).send({ msg: "Route not found" });
 });
+
+app.use((err, req, res, next) => {
+  console.log(err);
+  
+  if (err.status && err.msg) {
+    res.status(err.status).send({ msg: err.msg });
+  }
+})
 
 
 
--- models/app.model.js
+++ models/app.model.js
@@ -1,10 +1,24 @@
 const express = require('express');
 const db = require('../db/connection');
 const app = require('../app');
+const { rows } = require('pg/lib/defaults');
 
 exports.fetchTopics = () => {
     return db.query('SELECT * FROM topics;')
         .then(({ rows: topics }) => {
             return topics;
         })
+}
+
+exports.fetchArticlesById = (article_id) => {
+    return db.query('SELECT * from articles where article_id = $1;', [article_id]).then(({ rows }) => {
+        const article = rows[0];
+        if (!article) {
+            return Promise.reject({
+                status: 404,
+                msg: `No article found with article_id ${article_id}`
+            })
+        }
+        return article;
+    })
 }
