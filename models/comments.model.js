14d112aea085b5382ccc7bc19cb5a4ec491476ac
wbaig2 <w.baig321@gmail.com>
2022-08-25 16:28:25 +0100

    add comment display order

--- models/comments.model.js
+++ models/comments.model.js
@@ -2,7 +2,7 @@ const db = require("../db/connection");
 const { checkIfArticleIdExists, checkIfCommentIdExists } = require("../db/seeds/utils");
 
 exports.fetchCommentsByArticleId = (article_id) => {
-  return db.query("SELECT * FROM comments where article_id = $1;", [article_id]).then(({ rows: comments }) => {
+  return db.query("SELECT * FROM comments where article_id = $1 ORDER BY created_at desc;", [article_id]).then(({ rows: comments }) => {
     if (comments.length === 0) {
       return checkIfArticleIdExists(article_id)
     }
