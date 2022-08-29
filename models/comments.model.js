52fb7545d09abe2cf451222b4e6aec18cc1a438d
wbaig2 <w.baig321@gmail.com>
2022-08-29 12:49:35 +0100

    amend fetch comments query to get author info

--- models/comments.model.js
+++ models/comments.model.js
@@ -2,7 +2,7 @@ const db = require("../db/connection");
 const { checkIfArticleIdExists, checkIfCommentIdExists } = require("../db/seeds/utils");
 
 exports.fetchCommentsByArticleId = (article_id) => {
-  return db.query("SELECT * FROM comments where article_id = $1 ORDER BY created_at desc;", [article_id]).then(({ rows: comments }) => {
+  return db.query("SELECT * FROM comments c INNER JOIN users u ON c.author = u.author where c.article_id = $1 ORDER BY c.created_at desc;", [article_id]).then(({ rows: comments }) => {
     if (comments.length === 0) {
       return checkIfArticleIdExists(article_id)
     }
